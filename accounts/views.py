f3f01fc56a37e958421d8698e26d4846a297682f
Yash Sonune <yash.sonune08@gmail.com>
2021-05-03 18:44:30 +0530

    #11 CRUD Operations

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .models import *
+from .forms import OrderForm
 # Create your views here.
 
 
@@ -37,3 +39,46 @@ def customer(request, pk):
         'orders': orders,
     }
     return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print(f"Printing Post: {request.POST}")
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    """
+    Deletes an Order
+    """
+    order = Order.objects.get(id=pk)
+    context = {
+        'item': order,
+    }
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    return render(request, 'accounts/delete.html', context)
